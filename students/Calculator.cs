14fdea42ba572d9854bb5e37dee3b2667e8242b4
SashaDutka <odutka@kse.org.ua>
2026-01-14 10:06:23 +0200

    added bag

---
+++ students/Calculator.cs
@@ -0,0 +1,12 @@
+namespace DefaultNamespace;
+
+public class Calculator
+{
+    public static void Main(string[] args)
+    {
+        int a = 2;
+        int b = 3;
+        int c = a+b;
+        Console.WriteLine ($"{c}");
+
+}
