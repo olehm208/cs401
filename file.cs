a84e8b4f99231e8dfb5c9c50936c398fafca38a4
Oleh Mystyk <79378965+olehm208@users.noreply.github.com>
2026-01-14 10:07:48 +0200

    Update and rename students/Calculator.cs to file.cs

--- file.cs
+++ file.cs
@@ -1,12 +1,13 @@
-namespace DefaultNamespace;
+using System;
 
-public class Calculator
+public class HelloWorld
 {
     public static void Main(string[] args)
     {
         int a = 2;
+        // Олег - тут має бути int b = 2;
         int b = 3;
         int c = a+b;
         Console.WriteLine ($"{c}");
-
+    }
 }
